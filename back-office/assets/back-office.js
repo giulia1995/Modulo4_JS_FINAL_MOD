55caf534558bf314bc98929788f67e08f5b7ba6b
Giulia_vi95 <giulia.vaccaro95@gmail.com>
2024-01-25 20:53:52 +0100

    logica ok

--- back-office/assets/back-office.js
+++ back-office/assets/back-office.js
@@ -1,86 +1,89 @@
-const sendPost=document.getElementById("sendPost");
+const sendPost = document.getElementById("sendPost");
 sendPost.addEventListener("click", sendPostRequest);
 
-function sendPostRequest() {
-    const apiUrl = 'https://striveschool-api.herokuapp.com/api/product/';
-    const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWIwNDNmNGNlZjA0ODAwMTgxZGQ1Y2IiLCJpYXQiOjE3MDYwNTA1NDgsImV4cCI6MTcwNzI2MDE0OH0.hOw8a2XqcBA0CgNz8MmbrAlf3HC3b2xAA9cnHmBJBpI';
+async function sendPostRequest() {
+    try {
+        const apiUrl = 'https://striveschool-api.herokuapp.com/api/product/';
+        const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWIwNDNmNGNlZjA0ODAwMTgxZGQ1Y2IiLCJpYXQiOjE3MDYwNTA1NDgsImV4cCI6MTcwNzI2MDE0OH0.hOw8a2XqcBA0CgNz8MmbrAlf3HC3b2xAA9cnHmBJBpI';
 
-    const productName = document.querySelector('#productName').value;
-    const productDescription = document.querySelector('#productDescription').value;
-    const productBrand = document.querySelector('#productBrand').value;
-    const productImageURL = document.querySelector('#productImageURL').value;
-    const productPrice = document.querySelector('#productPrice').value;
+        const productName = document.querySelector('#productName').value;
+        const productDescription = document.querySelector('#productDescription').value;
+        const productBrand = document.querySelector('#productBrand').value;
+        const productImageURL = document.querySelector('#productImageURL').value;
+        const productPrice = document.querySelector('#productPrice').value;
 
-    const productData = {
-        name: productName,
-        description: productDescription,
-        brand: productBrand,
-        imageUrl: productImageURL,
-        price: parseFloat(productPrice)
-    };
+        const productData = {
+            name: productName,
+            description: productDescription,
+            brand: productBrand,
+            imageUrl: productImageURL,
+            price: parseFloat(productPrice)
+        };
 
-    fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${apiKey}`
-        },
-        body: JSON.stringify(productData)
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Prodotto aggiunto con successo:', data);
-        })
-        .catch(error => {
-            console.error('Errore durante l\'aggiunta del prodotto:', error);
+        const response = await fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apiKey}`
+            },
+            body: JSON.stringify(productData)
         });
-}
-const sendGet=document.getElementById("sendGet");
-sendGet.addEventListener("click",sendGetRequest);
 
-function sendGetRequest() {
-    const apiUrl = 'https://striveschool-api.herokuapp.com/api/product/';
-    const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWIwNDNmNGNlZjA0ODAwMTgxZGQ1Y2IiLCJpYXQiOjE3MDYwNTA1NDgsImV4cCI6MTcwNzI2MDE0OH0.hOw8a2XqcBA0CgNz8MmbrAlf3HC3b2xAA9cnHmBJBpI';
-
-    fetch(apiUrl, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${apiKey}`
-        }
-    })
-        .then(response => response.json())
-        .then(data => {
-            const productList = document.createElement('ul');
-            data.forEach(product => {
-                const listItem = document.createElement('li');
-                productList.style.padding = '0';
-                listItem.style.margin = '5px 0px'
-                const simplifiedProduct = {
-                    _id: product._id,
-                    name: product.name
-                };
-                listItem.textContent = JSON.stringify(simplifiedProduct);
-                productList.appendChild(listItem);
-            });
-            const productListContainer = document.getElementById('productListContainer');
-            productListContainer.innerHTML = '';
-            productListContainer.appendChild(productList);
-
-            console.log('Dati ottenuti con successo:', data);
-        })
-        .catch(error => {
-            console.error('Errore durante la richiesta GET:', error);
-        });
+        const data = await response.json();
+        console.log('UPLOAD PRODUCT SUCCESS:', data);
+    } catch (error) {
+        console.error('FAILED ADD PRODUCT:', error);
+    }
 }
 
-const modify=document.getElementById("modify");
+const sendGet = document.getElementById("sendGet");
+sendGet.addEventListener("click", sendGetRequest);
+
+async function sendGetRequest() {
+    try {
+        const apiUrl = 'https://striveschool-api.herokuapp.com/api/product/';
+        const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWIwNDNmNGNlZjA0ODAwMTgxZGQ1Y2IiLCJpYXQiOjE3MDYwNTA1NDgsImV4cCI6MTcwNzI2MDE0OH0.hOw8a2XqcBA0CgNz8MmbrAlf3HC3b2xAA9cnHmBJBpI';
+
+        const response = await fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apiKey}`
+            }
+        });
+
+        const data = await response.json();
+
+        const productList = document.createElement('ul');
+        data.forEach(product => {
+            const listItem = document.createElement('li');
+            productList.style.padding = '0';
+            listItem.style.margin = '5px 0px';
+            const simplifiedProduct = {
+                _id: product._id,
+                name: product.name
+            };
+            listItem.textContent = JSON.stringify(simplifiedProduct);
+            productList.appendChild(listItem);
+        });
+
+        const productListContainer = document.getElementById('productListContainer');
+        productListContainer.innerHTML = '';
+        productListContainer.appendChild(productList);
+
+        console.log('DATA SUCCESS:', data);
+    } catch (error) {
+        console.error('ERROR DURING GET REQUEST:', error);
+    }
+}
+
+const modify = document.getElementById("modify");
 modify.addEventListener("click", showEditForm);
 
 function showEditForm() {
     const productIdInput = document.createElement('input');
     productIdInput.type = 'text';
-    productIdInput.placeholder = 'Inserisci l\'ID del prodotto da modificare';
+    productIdInput.placeholder = 'GET > INSERT ID PRODUCT TO EDIT';
     productIdInput.style.display = 'block';
     productIdInput.style.margin = '0 auto';
     productIdInput.style.maxWidth = '400px';
@@ -89,137 +92,150 @@ function showEditForm() {
 
     const searchButton = document.createElement('button');
     searchButton.type = 'button';
-    searchButton.textContent = 'CERCA';
+    searchButton.textContent = 'SEARCH';
     searchButton.style.display = 'block';
     searchButton.style.margin = 'auto';
 
     const deleteButton = document.createElement('button');
     deleteButton.type = 'button';
-    deleteButton.textContent = 'CANCELLA';
+    deleteButton.textContent = 'DELETE';
     deleteButton.style.display = 'block';
     deleteButton.style.margin = '10px auto';
-    deleteButton.style.backgroundColor = "red"
+    deleteButton.style.backgroundColor = "red";
 
-    deleteButton.onclick = () => {
+    deleteButton.onclick = async () => {
         const productId = productIdInput.value;
-        if (productId && confirm('Sei sicuro di voler eliminare il prodotto?')) {
-            sendDeleteRequest(productId);
+        if (productId && confirm('Are you sure to delete product?')) {
+            try {
+                await sendDeleteRequest(productId);
+            } catch (error) {
+                alert('Delete Failed.');
+            }
         } else {
-            alert('Inserisci un ID valido.');
+            alert('Please insert a valid ID.');
         }
     };
 
-    searchButton.onclick = () => {
+    searchButton.onclick = async () => {
         const productId = productIdInput.value;
         if (productId) {
             const apiUrl = `https://striveschool-api.herokuapp.com/api/product/${productId}`;
             const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWIwNDNmNGNlZjA0ODAwMTgxZGQ1Y2IiLCJpYXQiOjE3MDYwNTA1NDgsImV4cCI6MTcwNzI2MDE0OH0.hOw8a2XqcBA0CgNz8MmbrAlf3HC3b2xAA9cnHmBJBpI';
 
-            fetch(apiUrl, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${apiKey}`
-                }
-            })
-                .then(response => response.json())
-                .then(product => {
-                    const editForm = document.createElement('form');
-                    editForm.id = 'editProductForm';
-
-                    const fieldsToShow = ["name", "description", "brand", "imageUrl", "price"];
-
-                    fieldsToShow.forEach(key => {
-                        const label = document.createElement('label');
-                        label.setAttribute('for', key);
-                        label.textContent = `${key}:`;
-
-                        const input = document.createElement('input');
-                        input.setAttribute('type', 'text');
-                        input.setAttribute('id', key);
-                        input.setAttribute('name', key);
-                        input.value = product[key];
-
-                        editForm.appendChild(label);
-                        editForm.appendChild(input);
-                    });
-
-                    const updateButton = document.createElement('button');
-                    updateButton.setAttribute('type', 'button');
-                    updateButton.textContent = 'Aggiorna';
-                    updateButton.onclick = () => sendPutRequest(productId, fieldsToShow);
-
-                    editForm.appendChild(updateButton);
-
-                    document.body.appendChild(editForm);
-                })
-                .catch(error => {
-                    console.error('Errore durante la richiesta GET per la modifica:', error);
+            try {
+                const response = await fetch(apiUrl, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${apiKey}`
+                    }
                 });
+
+                const product = await response.json();
+
+                const editForm = document.createElement('form');
+                editForm.id = 'editProductForm';
+
+                const fieldsToShow = ["name", "description", "brand", "imageUrl", "price"];
+
+                fieldsToShow.forEach(key => {
+                    const label = document.createElement('label');
+                    label.setAttribute('for', key);
+                    label.textContent = `${key}:`;
+
+                    const input = document.createElement('input');
+                    input.setAttribute('type', 'text');
+                    input.setAttribute('id', key);
+                    input.setAttribute('name', key);
+                    input.value = product[key];
+
+                    editForm.appendChild(label);
+                    editForm.appendChild(input);
+                });
+
+                const updateButton = document.createElement('button');
+                updateButton.setAttribute('type', 'button');
+                updateButton.textContent = 'Upload';
+                updateButton.onclick = async () => {
+                    try {
+                        await sendPutRequest(productId, fieldsToShow);
+                    } catch (error) {
+                        console.error('Error during product upload:', error);
+                    }
+                };
+
+                editForm.appendChild(updateButton);
+
+                document.body.appendChild(editForm);
+            } catch (error) {
+                console.error('Error during PUT request to edit:', error);
+            }
         } else {
-            alert('Inserisci un ID valido.');
+            alert('Insert Valid ID.');
         }
     };
 
     document.body.appendChild(productIdInput);
     document.body.appendChild(searchButton);
     document.body.appendChild(deleteButton);
-
 }
 
-function sendPutRequest(productId, fields) {
+async function sendPutRequest(productId, fields) {
     const apiUrl = `https://striveschool-api.herokuapp.com/api/product/${productId}`;
     const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWIwNDNmNGNlZjA0ODAwMTgxZGQ1Y2IiLCJpYXQiOjE3MDYwNTA1NDgsImV4cCI6MTcwNzI2MDE0OH0.hOw8a2XqcBA0CgNz8MmbrAlf3HC3b2xAA9cnHmBJBpI';
 
     const updatedProductData = {};
 
     fields.forEach(field => {
-        updatedProductData[field] = document.querySelector("#field").value;
+        updatedProductData[field] = document.querySelector(`#${field}`).value;
     });
 
-    fetch(apiUrl, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${apiKey}`
-        },
-        body: JSON.stringify(updatedProductData)
-    })
-        .then(response => {
-            if (response.ok) {
-                console.log('Prodotto aggiornato con successo.');
-            } else {
-                console.error('Errore durante l\'aggiornamento del prodotto.');
-            }
-        })
-        .catch(error => {
-            console.error('Errore durante la richiesta PUT:', error);
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apiKey}`
+            },
+            body: JSON.stringify(updatedProductData)
         });
+
+        if (response.ok) {
+            console.log('Prodotto aggiornato con successo.');
+        } else {
+            console.error('Errore durante l\'aggiornamento del prodotto.');
+        }
+    } catch (error) {
+        console.error('Errore durante la richiesta PUT:', error);
+    }
 }
-function sendDeleteRequest(productId) {
+
+async function sendDeleteRequest(productId) {
     const apiUrl = `https://striveschool-api.herokuapp.com/api/product/${productId}`;
     const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWIwNDNmNGNlZjA0ODAwMTgxZGQ1Y2IiLCJpYXQiOjE3MDYwNTA1NDgsImV4cCI6MTcwNzI2MDE0OH0.hOw8a2XqcBA0CgNz8MmbrAlf3HC3b2xAA9cnHmBJBpI';
 
-    fetch(apiUrl, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${apiKey}`
-        }
-    })
-        .then(response => {
-            if (response.ok) {
-                console.log('Prodotto eliminato con successo.');
-            } else {
-                console.error('Errore durante l\'eliminazione del prodotto.');
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apiKey}`
             }
-        })
-        .catch(error => {
-            console.error('Errore durante la richiesta DELETE:', error);
         });
+
+        if (response.ok) {
+            console.log('Prodotto eliminato con successo.');
+        } else {
+            console.error('Errore durante l\'eliminazione del prodotto.');
+        }
+    } catch (error) {
+        console.error('Errore durante la richiesta DELETE:', error);
+    }
 }
-const reset=document.getElementById("resetAll");
+
+const reset = document.getElementById("resetAll");
 reset.addEventListener("click", resetPage);
+
 function resetPage() {
     location.reload();
 }
--- home/assets/home.js
+++ home/assets/home.js
@@ -1,36 +1,44 @@
 const apiUrl = "https://striveschool-api.herokuapp.com/api/product/";
 const apiKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWIwNDNmNGNlZjA0ODAwMTgxZGQ1Y2IiLCJpYXQiOjE3MDYwNTA1NDgsImV4cCI6MTcwNzI2MDE0OH0.hOw8a2XqcBA0CgNz8MmbrAlf3HC3b2xAA9cnHmBJBpI`;
 
-fetch(apiUrl, {
-  headers: {
-    Authorization: `Bearer ${apiKey}`,
-  },
-})
-  .then((response) => response.json())
-  .then((data) => {
+async function fetchData() {
+  try {
+    const response = await fetch(apiUrl, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
     const productContainer = document.getElementById("product-container");
 
     data.forEach((product) => {
       const card = createProductCard(product);
       productContainer.appendChild(card);
     });
-  })
-  .catch((error) => {
+  } catch (error) {
     console.error("Error fetching data from the API:", error);
-  });
+  }
+}
+
 function createProductCard(product) {
   const card = document.createElement("div");
-  card.classList.add("col-sm-6", "col-md-3", "col-lg-2", "mb-4");
+  card.classList.add("col-sm-6", "col-md-3", "col-lg-4", "mb-4");
 
   card.innerHTML = `
-        <div class="card shadow p-3 mb-5 bg-body-black h-100">
-            <img src="${product.imageUrl}" class="card-img-top img-fluid" alt="${product.name}">
+        <div class="col card shadow p-3 mb-5 bg-body-black h-100">
+            <img src="${product.imageUrl}" id="imgcard"  class="card-img-top img-fluid" alt="${product.name}">
             <div class="card-body">
-                <h5 class="card-title">${product.name}</h5>
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#productModal" 
+                <h5 class="card-title fw-bold">${product.name}</h5>
+                <button type="button" class="btn btn-primary mt-3" data-bs-toggle="modal" data-bs-target="#productModal" 
                 onclick="openModal('${product.name}', '${product.description}', '${product.brand}', '${product.price}')">
                   Details
                 </button>
+                <a class="btn btn-success mt-3 text-white" href="/back-office/admin.html" role="button">Edit</a>
                 
             </div>
         </div>`;
@@ -47,3 +55,6 @@ function openModal(name, description, brand, price) {
         <p><strong>Price:</strong> $${price}</p>`;
 }
 openModal()
+
+// Chiamata alla funzione fetchData utilizzando async/await
+fetchData();
